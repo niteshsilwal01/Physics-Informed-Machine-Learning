2ae1c6cf39028aec086f3083daf5c1d73a2f51a2
nsilwal <niteshsilwal02@gmail.com>
2024-08-28 07:00:58 +0545

    Updated train function params

--- code_files/train.py
+++ code_files/train.py
@@ -7,14 +7,14 @@ import Plot_Save_Graph
 n_epochs = 10000
 
 # Training the model
-def train_and_save(model, optimizer, x_train, x_plot, u_analytical, epochs=n_epochs, plot_interval=200):
+def train_and_save(model, optimizer, x_train, x_plot, u_analytical, mu, k, epochs=n_epochs, plot_interval=200):
     for epoch in range(epochs):
 
         optimizer.zero_grad()
         y_p = model(x_plot)
 
         data_loss_val = torch.mean((u_analytical-y_p)**2)
-        physics_loss_val = Physics_Informed_Loss.physics_loss(model, x_train)
+        physics_loss_val = Physics_Informed_Loss.physics_loss(model, x_train, mu, k)
 
         loss = data_loss_val + physics_loss_val
 
